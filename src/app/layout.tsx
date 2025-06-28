575cf92d5c5ca473f89b31e23b9a50132b4d3a3a
severjason <ssever2010@gmail.com>
2025-06-28 23:46:03 +0300

    viewport adjustment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 
+import { Viewport } from 'next';
+
 import '@/styles/globals.css';
+
+export const viewport: Viewport = {
+  width: 'device-width, shrink-to-fit=no',
+  initialScale: 1,
+  maximumScale: 1,
+  minimumScale: 1,
+  userScalable: false,
+};
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   return (
